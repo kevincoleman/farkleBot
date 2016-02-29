879b9673af83341c7f352fbcc42463e484d5b105
kevincoleman <kevin.n.coleman@gmail.com>
2016-02-29 11:00:36 -0800

    rename

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 // local includes
 var DicePool = require('./lib/dicePool.js');
+var Score = require('./lib/score.js');
 
 var kevinsPool = new DicePool;
+var score = new Score;
 
-console.log(kevinsPool);
-kevinsPool.rollDice();
 console.log(kevinsPool.rolls);
+kevinsPool.rollDice();
+var lastRoll = kevinsPool.rolls[kevinsPool.rolls.length - 1];
+
+var vals = [];
+var quan = [];
+for (val in lastRoll) {
+  vals.push(lastRoll[val].value);
+  quan.push(lastRoll[val].quantity)
+}
+console.log(vals);
+console.log(quan);
+
+score.getBestScore(kevinsPool.rolls[kevinsPool.rolls.length - 1]);
